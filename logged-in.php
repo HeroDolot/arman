a8f0dd97a37598e94e537087a3cb440c50d91959
xMakiBoT <63221803+xMakiBoT@users.noreply.github.com>
2023-07-03 13:33:59 +0800

    Update logged-in.php

--- logged-in.php
+++ logged-in.php
@@ -1,21 +1,31 @@
 <?php
 session_start();
+include './connection.php';
 if (!isset($_SESSION["userid"])){
-    header("location:./index.php");
+	header("location:./index.php");
 }
-
+$name = mysqli_query($conn,"SELECT * FROM user WHERE id = ".$_SESSION["userid"])->fetch_assoc()["uname"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login Form</title>
+	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
-
-<body class="p-5">
-    <h1>Hello World('print')</h1>
+<body class="p-3 text-center">
+	<h1>Welcome, <?php echo $name;?></h1>
+	<div class="row card justify-content-center">
+		<div class="card-body col-md-6">
+			<h3>Add Information</h3>
+			<div class="input-group mb-3">
+				<span class="input-group-text">Name</span>
+				<input type="text" class="form-control" placeholder="First Name">
+				<input type="text" class="form-control" placeholder="Middle Name">
+				<input type="text" class="form-control" placeholder="Last Name">
+			</div>
+		</div>
+	</div>
 </body>
 </html>
